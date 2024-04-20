336708e814eae4a20cb1dee6f43f90948939fe58
Kosuke Takeda <kosuke050204@gmail.com>
2024-04-20 17:09:25 +0900

    Initial Commit

---
+++ sample-camera-app/sample_camera_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_camera_appApp.swift
+//  sample-camera-app
+//
+//  Created by Kosuke Takeda on 2024/04/20.
+//
+
+import SwiftUI
+
+@main
+struct sample_camera_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
